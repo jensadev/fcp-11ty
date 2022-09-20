11f2116d4042b44ca3c7bcd6095e08453c314791
jensnti <jens.andreasson@ntig.se>
2022-09-20 09:44:50 +0200

    ✨ year shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy({ './src/assets/images/': '/images' });
     eleventyConfig.addPassthroughCopy({ './src/assets/favicons': '/' });
+
+    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
     return {
         dir: {
             input: 'src',
